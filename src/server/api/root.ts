02a9072ec138045e2ac982f8f214e693b5120f67
Theo Browne <me@t3.gg>
2022-12-04 16:26:43 -0800

    Restructure folders for server

---
+++ src/server/api/root.ts
@@ -0,0 +1,16 @@
+import { router } from "./trpc";
+import { authRouter } from "./routers/auth";
+import { exampleRouter } from "./routers/example";
+
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here
+ */
+export const appRouter = router({
+  example: exampleRouter,
+  auth: authRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
